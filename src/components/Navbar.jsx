93eaede1e0a2fd31cd9b4f855270ee652beedcaf
sachweb <deeprajput392@gmail.com>
2026-07-05 10:55:37 +0530

    update in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,24 @@
+// import { Github } from "lucide-react";
+
 function Navbar() {
   return (
-    <nav className="bg-blue-600 shadow-lg">
-      <div className="max-w-6xl mx-auto px-6 py-4 flex justify-between">
-        <h1 className="text-white text-2xl font-bold">
-          Todo App 🚀
-        </h1>
-
-        <p className="text-white">
-          CI/CD Learning
-        </p>
+    <nav className="border-b border-slate-800 bg-slate-900">
+      <div className="max-w-6xl mx-auto flex justify-between items-center px-5 py-4">
+        <div>
+          <h1 className="text-2xl font-bold text-white">
+            🚀 React Todo CI/CD
+          </h1>
+        </div>
+{/* 
+        <a
+          href="https://github.com/"
+          target="_blank"
+          rel="noreferrer"
+          className="flex items-center gap-2 bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 rounded-lg transition"
+        >
+          <Github size={18} />
+          GitHub
+        </a> */}
       </div>
     </nav>
   );
